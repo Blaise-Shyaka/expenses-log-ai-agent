cf698ea5e97d69a20d8a667b998c19ef101fb8ff
Blaise Pascal Shyaka <blaiseshyaka95@gmail.com>
2026-07-01 14:41:00 +0200

    Improve compose file (#19)
    * Improved compose file
    
    * Added types to agent and api
    
    * Removed unused types
    
    * Worked on copilot review
    
    * Fixed issue with an infinite loop when calling tools
    
    * Added the ability to use ollama models locally and improved the prompt
    
    * Fixed a few formatting issues
    
    * Added a couple of files in gitignore
    
    * Switch from pip to uv
    
    * Reflected feedback
    
    * Fixed port mapping
    
    * Used uvicorn

--- expenses-api/main.py
+++ expenses-api/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from api.v1.endpoints.category import router as category_router
 from api.v1.endpoints.expense import router as expense_router
+import uvicorn
+import os
 
 app = FastAPI(title="Expenses Tracker API")
 
 app.include_router(expense_router, prefix="/api/v1/expenses", tags=["Expenses"])
 app.include_router(category_router, prefix="/api/v1/categories", tags=["Categories"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
